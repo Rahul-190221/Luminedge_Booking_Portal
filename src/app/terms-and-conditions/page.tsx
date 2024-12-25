1d5584d3ebf113dc1ec255bc97d03f44034407c9
Md Rahul Islam <mri190221@gmail.com>
2024-12-26 05:30:15 +0600

    termsand conditions

--- src/app/terms-and-conditions/page.tsx
+++ src/app/terms-and-conditions/page.tsx
@@ -1,125 +1,201 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 
 const TermsAndConditions = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center py-8 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-6xl w-full bg-white shadow-lg rounded-lg p-6 sm:p-8">
-        <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
-          Luminedge Policies
+    <div className="flex flex-col items-center w-full max-w-screen-xl mx-auto space-y-6 px-4 sm:px-8 md:px-12 font-montserrat">
+      {/* Terms and Conditions Section */}
+      <div className="w-full max-w-5xl bg-transparent p-6">
+        <h1
+          className="text-3xl font-semibold mb-2 text-center"
+          style={{
+            color: "#face39",
+            fontSize: isMobile ? "1.5rem" : "2rem",
+          }}
+        >
+          Mock Test Terms and Conditions
         </h1>
-
-        <section id="terms-conditions" className="mb-6">
-          <h2 className="text-xl font-semibold text-gray-800 mb-2">
-            Terms and Conditions
-          </h2>
-          <p className="text-gray-700 mb-4">
-            Welcome to Luminedge Bangladesh! These Terms and Conditions
-            (“Terms”) govern your use of our services provided through our
-            website Luminedge Bangladesh. By using our services, you agree to
-            comply with these Terms. If you disagree with any part of these
-            Terms, please do not use our services.
+        <div className="space-y-3">
+          <ol className="list-decimal list-inside">
+            <li className="mb-4">
+              <strong>Mock Test Purchase:</strong> All mock tests must be
+              pre-purchased and fully paid before booking. No due payments will
+              be accepted at the time of booking or on the test date.
+            </li>
+            <li className="mb-4">
+              <strong>Booking Timeline:</strong> Mock tests must be booked{" "}
+              <span className="font-semibold">at least 24 hours in advance</span>
+              , depending on availability for the desired date.
+            </li>
+            <li className="mb-4">
+              <strong>Valid ID Requirements:</strong> Candidates must present a
+              valid <span className="font-semibold">NID or Passport</span> on the
+              test day. A clear image of the ID document is acceptable if the
+              physical copy is unavailable.
+            </li>
+            <li className="mb-4">
+              <strong>Accurate Information:</strong> Candidate details, including{" "}
+              <span className="font-semibold">
+                Name, Money Receipt Number, and NID/Passport Information
+              </span>
+              , must be valid and match exactly with the information provided at
+              the time of booking. Any mismatch may result in the candidate being
+              unable to take the test.
+            </li>
+            <li className="mb-4">
+              <strong>Cancellation and Rescheduling:</strong> <br />
+              <ul className="list-disc list-inside ml-6">
+                <li className="mb-4">
+                  <strong>Cancellation Policy:</strong> Booked mock tests{" "}
+                  <span className="font-semibold">cannot be canceled.</span>
+                </li>
+                <li className="mb-4">
+                  <strong>Rescheduling Policy:</strong>
+                  <ul className="list-disc list-inside ml-6">
+                    <li className="mb-4">
+                      Rescheduling requests must be made{" "}
+                      <span className="font-semibold">
+                        at least 24 hours before the booked test date
+                      </span>
+                      .
+                    </li>
+                    <li className="mb-4">
+                      Rescheduling depends on the availability of new test dates.
+                    </li>
+                    <li className="mb-4">
+                      If the desired test date is unavailable, Luminedge is not
+                      liable, and <span className="font-semibold">no refunds</span>{" "}
+                      will be issued.
+                    </li>
+                    <li className="mb-4">
+                      Rescheduling is not allowed within 24 hours of the test
+                      date.
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li className="mb-4">
+              <strong>No-Show Policy:</strong> Failing to appear for the test on
+              the scheduled date and time will be considered a{" "}
+              <span className="font-semibold">no-show,</span> and the mock test
+              will be treated as taken. No refunds or rescheduling options will
+              be provided in such cases.
+            </li>
+            <li className="mb-4">
+              <strong>Punctuality:</strong> Candidates must report to the
+              Luminedge office{" "}
+              <span className="font-semibold">
+                at least 30 minutes before the scheduled test time
+              </span>
+              . Late arrivals may result in automatic cancellation of the mock
+              test. No refunds or rescheduling options will be provided in such
+              cases.
+            </li>
+            <li className="mb-4">
+              <strong>Mock Test Process:</strong>
+              <ul className="list-disc list-inside ml-6">
+                <li className="mb-4">
+                  Candidates must follow all instructions provided by the
+                  invigilator during the test.
+                </li>
+                <li className="mb-4">
+                  Disruptive or inappropriate behavior may lead to
+                  disqualification from the mock test without any refund.
+                </li>
+              </ul>
+            </li>
+            <li className="mb-4">
+              <strong>Confidentiality:</strong> Mock test content and questions
+              are proprietary and must not be shared, copied, or reproduced in
+              any form.
+            </li>
+            <li className="mb-4">
+              <strong>Mock Test Format and Rules:</strong>
+              <ul className="list-disc list-inside ml-6">
+                <li>
+                  Mock tests are designed to simulate actual test conditions and
+                  formats.
+                </li>
+                <li>
+                  Candidates must bring their own stationery or necessary items if
+                  required (e.g., pencils, sharpener, eraser).
+                </li>
+              </ul>
+            </li>
+            <li className="mb-4">
+              <strong>Technical Issues:</strong> In case of technical or system
+              errors, Luminedge will make every effort to resolve the issue.
+              However, Luminedge is not liable for external disruptions like
+              internet outages or power failures.
+            </li>
+            <li className="mb-4">
+              <strong>Emergency Situations:</strong> In case of emergencies,
+              candidates must inform Luminedge immediately using the provided
+              contact details.
+            </li>
+          </ol>
+          <p>
+            By booking a mock test, candidates agree to adhere to all terms and
+            conditions mentioned above. Any violation of these terms may result
+            in cancellation or forfeiture of the mock test without a refund.
           </p>
+        </div>
+      </div>
 
-          <h3 className="text-lg font-semibold text-gray-800 mb-2">
-            Definitions
-          </h3>
-          <ul className="list-disc pl-6 text-gray-700 mb-4">
-            <li>
-              <strong>“We”, “Us”, “Our”:</strong> Refers to Luminedge Bangladesh
-              (Luminedge Limited), a limited liability company registered in
-              Bangladesh.
-            </li>
-            <li>
-              <strong>“You”, “Your”:</strong> Refers to the user or customer of
-              our services.
-            </li>
-            <li>
-              <strong>“Services”:</strong> Refers to study abroad consultancy
-              and English language proficiency training provided by Luminedge
-              Bangladesh.
-            </li>
-            <li>
-              <strong>“Website”:</strong> Refers to{" "}
-              <a
-                href="https://luminedge.com.bd"
-                className="text-blue-600 hover:underline"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                https://luminedge.com.bd
-              </a>
-              .
-            </li>
-          </ul>
+      {/* Emergency Contact Information */}
+      <div className="w-full max-w-5xl bg-transparent p-3">
+        <h1
+          className="text-xl font-semibold text-center"
+          style={{ color: "#face39" }}
+        >
+            {isMobile ? "Emergency Contact Info" : "Emergency Contact Information"}
+        </h1>
+        <p className="text-center mt-1 mb-4">
+          In case of any emergency or if you need to communicate with us regarding
+          your mock test, please contact:
+        </p>
+        {/* Contact Info Section */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
+          <div className="p-4 border rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold mb-2">Address</h2>
+            <p>
+              Level 12, Gawsia Twin Peak, 743 Satmasjid Road, Dhanmondi, Dhaka,
+              Bangladesh, 1205
+            </p>
+          </div>
 
-          <h3 className="text-lg font-semibold text-gray-800 mb-2">
-            Services Provided
-          </h3>
-          <p className="text-gray-700 mb-4">We offer the following services:</p>
-          <ul className="list-decimal pl-6 text-gray-700 mb-4">
-            <li>
-              <strong>Study Abroad Consultancy:</strong> Personalized
-              consultancy for students seeking to study abroad, including
-              guidance on university selection, application processes, and visa
-              procedures.
-            </li>
-            <li>
-              <strong>English Language Proficiency Training:</strong> Training
-              programs to improve your English language skills, including
-              preparation for tests such as IELTS and TOEFL.
-            </li>
-          </ul>
+          <div className="p-4 border rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold mb-2">Telephone</h2>
+            <p>01400-403474</p>
+            <p>01400-403475</p>
+            <p>01400-403486</p>
+            <p>01400-403487</p>
+            <p>01400-403493</p>
+            <p>01400-403494</p>
+          </div>
 
-          <h3 className="text-lg font-semibold text-gray-800 mb-2">
-            Registration and Accounts
-          </h3>
-          <p className="text-gray-700 mb-4">
-            You must provide accurate information when creating an account. You
-            agree not to share your account details and to safeguard your
-            account information.
-          </p>
-
-          <h3 className="text-lg font-semibold text-gray-800 mb-2">
-            Personal Information
-          </h3>
-          <ul className="list-disc pl-6 text-gray-700 mb-4">
-            <li>
-              <strong>Information We Collect:</strong> Email address, first
-              name, last name, and phone number.
-            </li>
-            <li>
-              <strong>Use of Information:</strong> We use your information to
-              improve services and communicate with you, without sharing it with
-              third parties except as required by law.
-            </li>
-          </ul>
-
-          <h3 className="text-lg font-semibold text-gray-800 mb-2">
-            Payment Terms
-          </h3>
-          <p className="text-gray-700 mb-4">
-            Payments are made at our official location. Fees and applicable
-            taxes will be communicated directly to you.
-          </p>
-
-          <h3 className="text-lg font-semibold text-gray-800 mb-2">
-            Contact Information
-          </h3>
-          <ul className="list-disc pl-6 text-gray-700 mb-4">
-            <li>
-              <strong>Email:</strong>{" "}
-              <a
-                href="mailto:info.dhaka@luminedge.com.au"
-                className="text-blue-600 hover:underline"
-              >
-                info.dhaka@luminedge.com.au
-              </a>
-            </li>
-            <li>
-              <strong>Phone:</strong> +88 01400 403474
-            </li>
-          </ul>
-        </section>
+          <div className="p-4 border rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold mb-2">E-mail</h2>
+            <p>ielts.luminedge@gmail.com</p>
+          </div>
+        </div>
       </div>
     </div>
   );
